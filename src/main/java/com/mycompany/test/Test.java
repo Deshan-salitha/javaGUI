95da18946d48a0394d3fb5c514a2f9b2df1bc8ad
Deshan-salitha <71576127+Deshan-salitha@users.noreply.github.com>
2022-04-20 11:47:59 +0530

    today task

---
+++ src/main/java/com/mycompany/test/Test.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.test;
+import java.awt.event.*;
+import java.awt.*;
+import javax.swing.*;
+import com.mycompany.test.form;
+/**
+ *
+ * @author pc
+ */
+public class Test {
+
+
+    public static void main(String[] args) {
+        
+        form f = new form();
+        f.setVisible(true);
+
+    }
+}
